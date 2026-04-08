4b95ff80506b28a0faf4bad23ce34118314319b0
Carlos Neto <106997150+carlosbne@users.noreply.github.com>
2026-04-08 15:49:52 +0000

    :white_check_mark: test: adicionado teste das questoes

---
+++ src/questao02.test.ts
@@ -0,0 +1,7 @@
+import { concatArray } from "./questao02";
+
+const array = ["Arrays", "com", "Typescript"]
+
+test('array concatenado:', () => {
+    expect(concatArray(array)).toEqual("Arrays com Typescript");
+});
